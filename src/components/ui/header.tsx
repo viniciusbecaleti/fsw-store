514fad2f5e3e62f71b2b6edb236c166bef10fdea
Vinicius Becaleti <vinicius@becaleti.com>
2023-10-20 17:03:11 -0300

    feat: add header component

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,13 +1,48 @@
-import { MenuIcon, ShoppingCartIcon } from "lucide-react";
+import {
+  HomeIcon,
+  ListOrderedIcon,
+  LogInIcon,
+  MenuIcon,
+  PercentIcon,
+  ShoppingCartIcon,
+} from "lucide-react";
 import { Button } from "./button";
 import { Card } from "./card";
+import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
 
 const Header = () => {
   return (
     <Card className="flex items-center justify-between rounded-none border-l-0 border-r-0 border-t-0 p-[1.875rem]">
-      <Button size="icon" variant="outline">
-        <MenuIcon />
-      </Button>
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button size="icon" variant="outline">
+            <MenuIcon />
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="left">
+          <SheetHeader className="text-left text-lg font-semibold">
+            Menu
+          </SheetHeader>
+
+          <div className="mt-2 flex flex-col gap-2">
+            <Button variant="outline" className="w-full justify-start gap-3">
+              <LogInIcon size={16} /> Fazer Login
+            </Button>
+
+            <Button variant="outline" className="w-full justify-start gap-3">
+              <HomeIcon size={16} /> Início
+            </Button>
+
+            <Button variant="outline" className="w-full justify-start gap-3">
+              <PercentIcon size={16} /> Ofertas
+            </Button>
+
+            <Button variant="outline" className="w-full justify-start gap-3">
+              <ListOrderedIcon size={16} /> Catálogo
+            </Button>
+          </div>
+        </SheetContent>
+      </Sheet>
 
       <h1 className="text-lg font-semibold">
         <span className="text-primary">FSW</span> Store
