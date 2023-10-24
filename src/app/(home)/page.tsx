1b7f2d49d618c9713110f79198fa6f316c45c089
Vinicius Becaleti <vinicius@becaleti.com>
2023-10-24 20:25:32 -0300

    feat: add first banner to home page

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default async function Home() {
+  return (
+    <div className="p-5">
+      <Image
+        src="/banner-home-01.png"
+        alt="Até 55% de desconto só esse mês"
+        width={0}
+        height={0}
+        sizes="100vw"
+        className="h-auto w-full"
+      />
+    </div>
+  );
+}
