168f24843a196c3b9e7c3bc7be811055c4fe9d51
Vinicius Becaleti <vinicius@becaleti.com>
2023-10-24 23:01:49 -0300

    feat: add productWithTotalPrice util function

---
+++ src/utils/product.ts
@@ -0,0 +1,25 @@
+import { Product } from "@prisma/client";
+
+interface ProductWithTotalPrice extends Product {
+  totalPrice: number;
+}
+
+export const productWithTotalPrice = (
+  product: Product,
+): ProductWithTotalPrice => {
+  if (product.discountPercentage === 0) {
+    return {
+      ...product,
+      totalPrice: Number(product.basePrice),
+    };
+  }
+
+  const totalPrice =
+    Number(product.basePrice) -
+    Number(product.basePrice) * (product.discountPercentage / 100);
+
+  return {
+    ...product,
+    totalPrice,
+  };
+};
